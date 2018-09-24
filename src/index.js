b2d5f764a404d5a70fd1259c4271d496ad4ef4b6
Karinka <halavach.k@mail.ru>
2018-09-24 23:18:46 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let arr = equation.split(" ");
+  let a = +arr[0];
+  let b = +(arr[3] + arr[4]);
+  let c = +(arr[7] + arr[8]);
+  let disc = b * b - 4 * a * c;
+  let x1 = Math.round((-b + Math.sqrt(disc)) / (2 * a));
+  let x2 = Math.round((-b - Math.sqrt(disc)) / (2 * a));
+  if (a > 0) {
+  	return [x2, x1];
+  }
+  return [x1, x2];
+
 }
